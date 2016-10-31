16bc7e1be5a82253f8a168fba219fb5f03eb41f4
leandroamore <leandro.amore@prisma.cc>
2016-10-31 17:15:56 -0300

    :-1:

--- index.php
+++ index.php
@@ -67,9 +67,9 @@ if(count($registrants) > 0) {
     echo "<tr><th>Name</th>";
     echo "<th>Email</th>";
     echo "<th>Date</th></tr>";
-	echo "var connstr" .$connstr.;
-	echo "var conn" .$conn.;
-	echo "original sqlsrv:Server= $host ; Database = $db " .$user.;
+	echo "var connstr" //.$connstr.;
+	//echo "var conn" .$conn.;
+	//echo "original sqlsrv:Server= $host ; Database = $db " .$user.;
     foreach($registrants as $registrant) {
         echo "<tr><td>".$registrant['name']."</td>";
         echo "<td>".$registrant['email']."</td>";
