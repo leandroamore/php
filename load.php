3ea0b9be66954c0d1659861a0792674b0ba5754f
leandroamore <leandro.amore@prisma.cc>
2016-10-31 20:53:09 -0300

    funcion

--- load.php
+++ load.php
@@ -1,31 +1,14 @@
+<html>
 <?php
-function get_server_load() {
-    
-        if (stristr(PHP_OS, 'win')) {
-        
-            $wmi = new COM("Winmgmts://");
-            $server = $wmi->execquery("SELECT LoadPercentage FROM Win32_Processor");
-            
-            $cpu_num = 0;
-            $load_total = 0;
-            
-            foreach($server as $cpu){
-                $cpu_num++;
-                $load_total += $cpu->loadpercentage;
-            }
-            
-            $load = round($load_total/$cpu_num);
-            
-        } else {
-        
-            $sys_load = sys_getloadavg();
-            $load = $sys_load[0];
-        
-        }
-        
-        return (int) $load;
-    
-    }
-$valor=get_server_load();
-echo $valor;
-?> 
+  function runMyFunction() {
+    echo 'I just ran a php function';
+  }
+
+  if (isset($_GET['hello'])) {
+    runMyFunction();
+  }
+?>
+
+Hello there!
+<a href='index.php?hello=true'>Run PHP Function</a>
+</html>
