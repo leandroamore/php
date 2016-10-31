08db063b27bb0e7c4267bb796cf35e6f350d49b6
leandroamore <leandro.amore@prisma.cc>
2016-10-31 20:58:34 -0300

    otra mas

--- load.php
+++ load.php
@@ -1,14 +1,30 @@
-<html>
-<?php
-  function runMyFunction() {
-    echo 'I just ran a php function';
-  }
+$('#btn').click(function(){
 
-  if (isset($_GET['hello'])) {
-    runMyFunction();
-  }
+  $.ajax({
+    url:'test.php?call=true',
+    type:'GET',
+    success:function(data){
+    body.append(data);
+
+    }
+  });
+
+})
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.1/jquery.min.js"></script>
+<form  method='get' >
+  
+  <input type="button" id="btn" value="click">    
+  
+</form>
+
+<?php 
+if(isset($_GET['call'])){
+
+    function anyfunction(){
+    echo "added";
+
+     // your funtion code
+
+}
+}
 ?>
-
-Hello there!
-<a href='load?hello=true'>Run PHP Function</a>
-</html>
