015b7e382f6a69ab18137d04fa5bacc77e8c1fc0
leandroamore <leandro.amore@prisma.cc>
2016-10-31 17:23:19 -0300

    111

--- index.php
+++ index.php
@@ -62,9 +62,9 @@ $sql_select = "SELECT * FROM registration_tbl";
 $stmt = $conn->query($sql_select);
 $registrants = $stmt->fetchAll(); 
 if(count($registrants) > 0) {
-    echo "<h2>People who are registered:</h2>";
-	echo $conn;
+    //echo $conn;
 	echo $connstr;
+	echo "<h2>People who are registered:</h2>";
     echo "<table>";
     echo "<tr><th>Name</th>";
     echo "<th>Email</th>";
