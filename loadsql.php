0c4078d9d2f164437c733af7d971ef05dbbcbb4d
leandroamore <leandro.amore@prisma.cc>
2016-10-31 21:06:00 -0300

    sql

---
+++ loadsql.php
@@ -0,0 +1,23 @@
+<?php
+
+$connection_string = getenv("SQLAZURECONNSTR_DbConn");
+// Parse db connection string into variables
+$vars_string = str_replace(";","&",$connection_string);
+parse_str($vars_string);
+
+$entorno=getenv("Entorno");
+try {
+    $conn = new PDO( "sqlsrv:Server= $Server ; Database = $Database", $User, $Password);
+	
+    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+}
+catch(Exception $e){
+    die(var_dump($e));
+}
+$sql_select = "SELECT * FROM registration_tbl";
+for($i = 0; $i < 9999; $i++) {	
+	$stmt = $conn->query($sql_select);
+	$registrants = $stmt->fetchAll()
+
+}
+?>
